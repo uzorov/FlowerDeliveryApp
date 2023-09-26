f977d6a4e306a9b117d62cfc64b6d3d82f1b21a8
Uzorov Kirill <uzorov_02@mail.ru>
2023-09-26 19:14:28 +0300

    Shopping Cart Screen was added

--- app/src/main/java/com/ilukhina/uylia/flowerdeliveryapp/ui/activities/cart_activity/CartScreen.kt
+++ app/src/main/java/com/ilukhina/uylia/flowerdeliveryapp/ui/activities/cart_activity/CartScreen.kt
@@ -4,59 +4,108 @@ import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.guru.composecookbook.meditation.ui.spacerHeight10
 import com.ilukhina.uylia.flowerdeliveryapp.ui.MainViewModel
-import com.ilukhina.uylia.flowerdeliveryapp.ui.activities.main_activity.components.FlowerCard
+import com.ilukhina.uylia.flowerdeliveryapp.ui.activities.cart_activity.components.CartItem
 import com.ilukhina.uylia.flowerdeliveryapp.ui.activities.main_activity.data.model.FlowerItem
 
 @Composable
 fun CartScreen(viewModel: MainViewModel) {
-    BasketSection(flowerItems = viewModel.getFlowerList(), viewModel = viewModel)
+
+    Scaffold(
+        bottomBar = {
+            PriceSection()
+        },
+        content = { innerPadding ->
+            BasketSection(
+                flowerItems = viewModel.getFlowerList(),
+                viewModel = viewModel,
+                Modifier.padding(innerPadding)
+            )
+        }
+    )
+
+
 }
 
 @Composable
-fun BasketSection(flowerItems: MutableState<List<FlowerItem>>, viewModel: MainViewModel) {
+fun PriceSection(sectionHeight: Dp = 70.dp) {
+    Surface(
+        color = Color.White,
+        contentColor = Color.Black
+    ) {
+        Divider()
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .height(sectionHeight)
+                .selectableGroup()
+                .padding(horizontal = 20.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        )
+        {
+            Text("Сумма заказа")
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Оформить заказ", color = Color.White)
+            }
+        }
+    }
+}
+
+@Composable
+fun BasketSection(
+    flowerItems: MutableState<List<FlowerItem>>,
+    viewModel: MainViewModel,
+    padding: Modifier
+) {
     val context = LocalContext.current
 
-    Column {
+    Column (modifier = padding) {
         LazyColumn(
             contentPadding = PaddingValues(horizontal = 7.dp),
         ) {
             items(flowerItems.value.size) {
-                FlowerCard(
+                CartItem(
                     flowerItem = flowerItems.value[it], Modifier
-                        .padding(end = 7.5.dp)
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
                         .aspectRatio(1f)
                         .clip(RoundedCornerShape(10.dp))
-                        .background(Color.Green.copy(0.1F))
-                        .wrapContentHeight()
+                        .background(Color.Blue.copy(0.1F))
                         .wrapContentWidth()
+                        .align(Alignment.CenterHorizontally)
                         .clickable {
                             viewModel.deleteFlowerItem(flowerItems.value[it])
                             Toast
                                 .makeText(context, "Букет удален из корзины", Toast.LENGTH_SHORT)
                                 .show()
-                            Log.d("TEST",
+                            Log.d(
+                                "TEST",
                                 viewModel
                                     .getFlowerList()
                                     .toString()
